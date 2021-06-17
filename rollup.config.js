3a15e104a69e3292ebb2689dcfdea28aa2d8f200
yklydxtt <1662106538@qq.com>
2021-06-17 14:15:06 +0800

    fix: change file name

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import { terser } from "rollup-plugin-terser";
 export default {
     input:'index.js',
     output:{
-        file:'dv.mim.js',
+        file:'dv.min.js',
         format:'iife'
     },
     plugins: [terser()],
